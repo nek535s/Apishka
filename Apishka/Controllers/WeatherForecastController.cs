180666bf90e421c19ea642895bb8ea84a0453256
Nek535s <nek535s@gmail.com>
2024-02-15 18:12:33 +0300

    TestCommit

--- Apishka/Controllers/WeatherForecastController.cs
+++ Apishka/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace Apishka.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetAllWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
